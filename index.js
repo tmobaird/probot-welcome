98f42942f6e6b1f2677bb1bf323473f99aa1e4cb
Thomas Baird <tbaird@mail.umw.edu>
2017-07-16 22:59:35 -0400

    Simple enhancements. Adding basic test structure. Configuring xo.

--- index.js
+++ index.js
@@ -7,18 +7,18 @@
 * To get your plugin running against GitHub, see:
 * https://github.com/probot/probot/blob/master/docs/development.md
 */
-module.exports = (robot) => {
-  robot.on('pull_request.opened', async (context) => {
-    const author = context.payload["pull_request"].user.login;
+module.exports = robot => {
+  robot.on('pull_request.opened', async context => {
+    const author = context.payload.pull_request.user.login;
     const repo = context.payload.repository.name;
     const number = context.payload.number;
     const owner = context.payload.repository.owner.login;
 
     try {
-      const res = await context.github.search.issues({ q: `is:pr+repo:${owner}/${repo}+author:${author}` });
+      const res = await context.github.search.issues({q: `is:pr+repo:${owner}/${repo}+author:${author}`});
       if (res.data.total_count === 0) {
-        const body = "Hey! We've noticed this is your first pull request on this repo, welcome & congrats!";
-        context.github.issues.createComment({ repo, number, owner, body });
+        const body = 'Hey! We\'ve noticed this is your first pull request on this repo, welcome & congrats!';
+        context.github.issues.createComment({repo, number, owner, body});
         console.log('SUCCESS: Comment has been succesfully posted.');
       }
     } catch (err) {
---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Sample Test', function () {
+  it('1 should equal 1', function () {
+    assert.equal(1, 1);
+  });
+});
